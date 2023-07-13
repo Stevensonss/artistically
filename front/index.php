0b912fa4f4280a96194b626d20ec92ef595faa75
Stevensonss <smileyaddress@gmail.com>
2023-07-13 22:42:01 +0200

    setupProjet

---
+++ front/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Artistically</title>
+
+    <link rel="stylesheet" href="/front/css/style.css">
+    <script type="text/JavaScript" src="/front/js/script.js"></script>
+</head>
+<body>
+    <?php echo("Hello World !") ?>
+</body>
+</html>
